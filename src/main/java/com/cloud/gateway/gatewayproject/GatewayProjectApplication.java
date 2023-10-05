6c1592ab07c9f2cb814554ff2596db7dc0db7dbd
kydiazpe <kdiazped@emeal.nttdata.com>
2023-10-05 14:56:55 -0500

    primera prueba del gateway

---
+++ src/main/java/com/cloud/gateway/gatewayproject/GatewayProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.gateway.gatewayproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayProjectApplication.class, args);
+	}
+
+}
